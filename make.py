812239d9951102e93b6cd4792b61b5315b36c547
Paulo Coutinho <paulo@prsolucoes.com>
2019-03-23 17:22:22 -0300

    fix log on windows

--- make.py
+++ make.py
@@ -9,7 +9,7 @@ proj_path = ezored_path
 ezored_path = os.path.join(ezored_path, 'ezored')
 
 if not os.path.isdir(ezored_path):
-    print("\033[93m[INFO] Cloning ezored-core to '{0}':\033[0m".format(ezored_path))
+    print("Cloning ezored-core to '{0}':".format(ezored_path))
 
     subprocess.call([
         'git',
