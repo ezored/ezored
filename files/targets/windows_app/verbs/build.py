d449d444099e1236809ae303530f1a5be0729122
Paulo Coutinho <paulo@prsolucoes.com>
2019-03-24 07:50:27 -0300

    fix windows copy dll

--- files/targets/windows_app/verbs/build.py
+++ files/targets/windows_app/verbs/build.py
@@ -100,7 +100,8 @@ def run(params):
                     'bin'
                 )
 
-                file.copy_all_inside(deps_bin_dir, build_bin_dir)
+                if os.path.isdir(deps_bin_dir):
+                    file.copy_all_inside(deps_bin_dir, build_bin_dir)
     else:
         log.error('Arch list for "{0}" is invalid or empty'.format(
             target_name
