4cc04aadcdf2f58ae930a383e6656fc58f3431c9
Paulo Coutinho <paulo@prsolucoes.com>
2019-12-22 04:12:11 -0300

    xcframework support

--- projects/ios/Sample/Sample/UI/Controllers/HomeViewController.swift
+++ projects/ios/Sample/Sample/UI/Controllers/HomeViewController.swift
@@ -153,7 +153,7 @@ class HomeViewController: BaseTableViewController {
                 let todo = EZRDomainTodo(
                     id: 0,
                     title: String(format: "Title %i", i),
-                    body: String(format: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. %i", i),
+                    body: String(format: "New TODO item description: %i", i),
                     data: [:],
                     done: false,
                     createdAt: Date(),
