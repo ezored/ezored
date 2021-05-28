5625807ddb5525a5a749cfc6375fdf4a8e5831e7
Paulo Coutinho <paulo@prsolucoes.com>
2021-05-28 03:47:08 -0300

    version 2 #27

--- files/targets/android/cmake/ndk-stl-config.cmake
+++ files/targets/android/cmake/ndk-stl-config.cmake
@@ -1,9 +1,7 @@
 # Copy shared STL files to Android Studio output directory so they can be packaged in the APK. Usage:
 #
 # find_package(ndk-stl REQUIRED)
-#
 # or
-#
 # find_package(ndk-stl REQUIRED PATHS ".")
 
 if(NOT ${ANDROID_STL} MATCHES "_shared")
@@ -12,6 +10,7 @@ endif()
 
 function(configure_shared_stl lib_path so_base)
     message("Configuring STL ${so_base} for ${ANDROID_ABI}")
+    
     configure_file(
         "${ANDROID_NDK}/sources/cxx-stl/${lib_path}/libs/${ANDROID_ABI}/lib${so_base}.so"
         "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/lib${so_base}.so" COPYONLY
